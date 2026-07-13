7219a0d26a992acde6b8523c76137d5ec7ca8b91
Everilll <averil_mauladani_33rpl@student.smktelkom-mlg.sch.id>
2026-07-13 19:31:50 +0700

    feat(backend): implement cloudinary upload service with image optimization

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { UploadModule } from './upload/upload.module';
+import { ModuleService } from './module/module.service';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, UploadModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ModuleService],
 })
 export class AppModule {}
